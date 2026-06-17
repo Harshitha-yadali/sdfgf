2700bdd9877655f7a16db9689d0842ba64144402
Harshitha-yadali <harshithayadali30@gmail.com>
2026-06-17 18:01:59 +0530

    Updated MapLocationPicker.tsx

--- src/components/MapLocationPicker.tsx
+++ src/components/MapLocationPicker.tsx
@@ -34,7 +34,7 @@ function loadGoogleMaps(): Promise<void> {
   if (gmapsLoadPromise) return gmapsLoadPromise;
   gmapsLoadPromise = new Promise<void>((resolve, reject) => {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${GMAPS_KEY}&libraries=places&loading=async`;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${GMAPS_KEY}&libraries=places`;
     script.async = true;
     script.onload = () => resolve();
     script.onerror = () => reject(new Error('Failed to load Google Maps'));
